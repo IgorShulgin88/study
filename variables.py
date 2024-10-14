0ee8a4c9a4ac0688f80fc7e02551a4b93ab647f2
IgorShulgin88 <uzepofig@gmail.com>
2024-10-14 07:36:34 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,5 @@
+Number_of_completed_hw=12
+Number_of_hours_spent=1.5
+Course_name='Python'
+Time_for_one_task=print(Number_of_hours_spent/Number_of_completed_hw)
+print(Course_name,',''всего задач:',Number_of_completed_hw,',затрачено часов:',Number_of_hours_spent,',среднее время выполнения',Number_of_hours_spent/Number_of_completed_hw)
